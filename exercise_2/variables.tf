21f8279805d033652808c38c761858cc2c4c4c25
KinnonYee-harness <169066316+KinnonYee-harness@users.noreply.github.com>
2025-05-27 16:22:11 -0400

    Update variables.tf

--- exercise_2/variables.tf
+++ exercise_2/variables.tf
@@ -1,14 +1,14 @@
 variable "access_key" {
   type        = string
-  default     = "ASIA5KL7XUCJLDNQW2SY"
+  default     = "ASIA5KL7XUCJOLW2PXYZ"
 }
 variable "secret_key" {
   type        = string
-  default     = "1kisgKs6Pyq3CQ0kBpmvRczOfqZG9CjLf6O5knOD"
+  default     = "vqMdbPQDetnUu0jfkMrLEDVgRce4Wn853xwvlwZ5"
 }
 variable "token_key" {
   type        = string
-  default     = "IQoJb3JpZ2luX2VjEJ3//////////wEaCXVzLWVhc3QtMSJIMEYCIQCPcQKe71UvFmFFhmfUtfOAy+cTVVJhH1RmOeKhBcTnbwIhALO/Y8E64m5WIXVu2qjwv3OpCPRYZQEwWpX6Tw3uNwj+KokDCGUQAxoMOTE1NjMyNzkxNjk4IgxSMXdutXM9BDL2rhQq5gIS1jQXFcotWZvSklPfkxsW9HWxNHvtj99ku5RdcrRWKrbDsF2E6/ts45TNa489e9zXE3DbGiqMLiaKovp9Dy9MwPUhozZPNWKSlXohJDBi1w+6AhY8t+FptO/t0CatAOkOiRWVIzy0SQ3zWlLMkNaemyszfD4XWniN9n4wa2coq1sZbq0jNbvthcqoYG91GSGk0Z0d986OFk6sqFDh9ao8idGjYXG7BswQFHvo+E2nK7Z2GdmWh05xUEhIx60tDXmGeEYDDhJewNwwnl6L1RcpLb1IeXvF6FwQUKbmSxy8m/ED8gQfnc8Dawv0TZWqlpBKEAZDsd+0YsOOnxQL1xM+mdThBrVRIv9c2fTPZ4VOA1rpmE+o2tMN2CAnrt8Mz/aiDco2LxjSv41B6/KsFBNQlbwfyORpvvdlyMmaM9K9zpygNSF7hbqQAj+IeyKtbVSCBoWrIabquB7jjXY3pNZ3BRZzvXX+MNy22MEGOqUBTBgWUlXOmiUwMmSx1USMGo+qtgXpQYGYIpy2Vt6OE4YxvX+2UJ5NtziCMNiAy9QyWgsnL289c7v04dHLIZOc5GLk/JOAFfoF2ECzhUmoxJV5NQCpXmkgC5e1d8876FUo9tnzRXi4Sptm/+bxKPQRq3TmEdZQJzLsraSZerQsv/M+9DUOlsO5dSlSsahzTqfIxI3XYzpfxb4cAxOYFp6fBD3/9Vjm"
+  default     = "IQoJb3JpZ2luX2VjEJ3//////////wEaCXVzLWVhc3QtMSJHMEUCIQC7Ypg3k1nnUNP/pL5YQzq7L7ZUcPweeJD8TG83qz8mjQIgRpkRLo0EgKvHO1JYGqxFKvSXlYnMXd84pWuqkItcUaEqiQMIZRADGgw5MTU2MzI3OTE2OTgiDGmbLG2BH8ujB7QdFCrmAhxRx/xkTcvipWbxrInAsDuMs9FAe5Qsc4Hylid/8g3yS2PYD/kOH926LE8xt/dbu52kT1QSAv0mTZ77A5SOwX1kCKm5rtAn2w41qPQpve0MUqRsN3zLGpckV8HMYu+zPtYpMoLuOLJPeGhQ8dyfef/soJ6K8nS028wEcjle9VErtkj4Fo+8HTzGAWogVEkmq/MbrrrtD/vbwCfdMkYiObQwz3ASu1+u6Kof7WfHp1NPdVVtao/tmT9QVJ4HS31OXWVXEZmfuoCoGuRB/cSj0S5BlPPoU5VRnsIjtKpS/Mhh7XKDXXvDd7S/TtmMpSOaOk//zZxZZHDqeHtWu8DjseexJId8BMLsALh59eGPZqHL3MzvApCl2Edl53ono3IjHbZYbMtq3kG1J95bGm/Th01aQ9KQfSFrGFWCIf4Wm2ng/0+fJ1H785kwyh525kwqK9TSoLQ3ismNDHVn53fordsKtmasCCIwu7nYwQY6pgFgeG6DEefVZ7pvRljD3mbqcAK24zI6Wr6oppcwdj9UvUp3IHiANb1UwJomof/+UG9nPVCR2eIBGnydzWU6urYDMenYKA+h7zgYEIUI4Xjlx4/ZiZD0rLJqZrrVTCSq85S7EKYFJE9o1lkt7fJoDAOyj2liqxAcL+W9ZSHFQ0tzqY8fZnYeqFdz+okTko9aaYgdutbpCLWSw5MFdpaIAi27vGaigxbd"
 }
 variable "region" {
   type        = string
