e2ba346fabe90c01e4b720d5d4dc2771c9dbc1fb
KinnonYee-harness <169066316+KinnonYee-harness@users.noreply.github.com>
2025-06-03 15:01:32 -0400

    Update variables.tf

--- exercise_2/variables.tf
+++ exercise_2/variables.tf
@@ -1,14 +1,14 @@
 variable "access_key" {
   type        = string
-  default     = ""
+  default     = "ASIA5KL7XUCJAWQBFZC2"
 }
 variable "secret_key" {
   type        = string
-  default     = ""
+  default     = "uWJHrI4y57DNq5jy1Hmem65ITllswk7Dqp5iPKVq"
 }
 variable "token_key" {
   type        = string
-  default     = ""
+  default     = "IQoJb3JpZ2luX2VjEEMaCXVzLWVhc3QtMSJHMEUCIQD2E6gcqJ1F4ytwYDeY87Pv9rCTKmyp6ZLVZRfY64PPCgIgUUAFGy8Q6bjU9MMLVBx2PWGVj6gl9LcRIbj0mhea+ekqiQMIHBADGgw5MTU2MzI3OTE2OTgiDDQheXYFm7s349c+2irmAoED+7tnSuwv4EefgogaduOSzlm6qGfSYOBiw/REmXrMh9Wvs6k2PlFUMlSxzT5n1DB9T8odGdPDFeNPxpJcrBkq5avNWOaV9gVUy0ad3C3IXjBYP5+FLDUyaJwYx+8zyrnjOMe/VR5Joi93Ic0SV6+iRunTN6P2Z8IpGbr8fDEZ5JkRg92OFpVEwW3cMqPAG+FxTdgnu6QhMYqikVRSpofa+ZXMmXtlU/AN+Yl+Tl8U7vhz92/L8k1YgBpPKVb16fsTNwkTyuGBYsarectnCQkg3gGoQOYRYX0fveUOyhtc7Duqucb8q0zIQqfZP4Slk6GeQY11urUw5fWtMYp+BK9gAm4oIZvG2kYRJOx6WZqhCKytrt0TKlLIkQAgEI+4EMec8LfOEY0AiSzW1OfiOhJCpMddU7T3xppvBtDlZMzhMWezov34GiD3wWEhZI+LT7+xFG05TKogWbrXuX1LIZKU3UVZT5Ew+Yz9wQY6pgH9xV8j8yFBZnmqtaF31tEsAbThq0ylgwBg5Jf8vRCx4qol2YVx4ruMdxzhOxw7BVViJEnrfKHH0qtBZpiFfK75QIh96Bf7NIcqXWxlUQbOg/YmvMZHmTWtgw7vT+XvDmidNdBs2SH0b73+toFs0EmttHAwaXxcEcukcj9U7KP3wbtjUJQAuOmjuqnLLRsYx3Q8yLdYlLp7XLuSo+ETktNNgJleAQQ+"
 }
 variable "region" {
   type        = string
